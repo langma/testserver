aff11db7f02fed3db946c3ef12f09b55d89e963d
Markus Lång <markus.lang@reaktor.fi>
2011-08-18 15:16:33 +0300

    change error percentage to be 0.1 instead of 100.

--- src/main/scala/TestServer.scala
+++ src/main/scala/TestServer.scala
@@ -1,5 +1,5 @@
 import test._
 
 object TestServer extends Application {
-  new JettyServer(new RequestHandler with JSONReplier with RandomErrors with Sleeping {val errorPercentage=100f}).start()
+  new JettyServer(new RequestHandler with JSONReplier with RandomErrors with Sleeping {val errorPercentage=0.1f}).start()
 }
